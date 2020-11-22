44bdbf16e62f3db5294a77b84410fd08bdb88b2c
Karolína Vyskočilová <karolina@kybernaut.cz>
2020-11-22 21:34:43 +0100

    Use package for package header

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
 
-clearConsole(); // Clear the console before the output
+welcome({
+    title: pkgJSON.name,
+    tagLine: `Howdy, nice to meet ya!`,
+    description: pkgJSON.description,
+    version: pkgJSON.version,
+    bgColor: `#FADC00`,
+    color: `#000000`,
+    bold: true,
+    clear: true,
+})
 
 console.log(`
 Karolína Vyskočilová - WordPress developer
